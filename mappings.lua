3a7190e21e200ba9c4541f77f5e205a39ed9fb1c
TheSaltyLiquorice <jonathan@jonsson.tech>
2023-04-10 10:29:50 +0200

    added move line command

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,10 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
@@ -20,9 +23,27 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<A-j>"] = { "<cmd> :m .+1 <CR>==", desc = "Move line down" },
+    ["<A-k>"] = { "<cmd> :m .-2 <CR>==", desc = "Move line up" },
+  },
+  i = {
+    ["<A-j>"] = { "<Esc>:m .+1<CR>==gi", desc = "Move line down" },
+    ["<A-k>"] = { "<Esc>:m .-2<CR>==gi", desc = "Move line up" },
+  },
+  v = {
+    ["<A-j>"] = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
+    ["<A-k>"] = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
 }
+
+
+-- nnoremap <A-j> :m .+1<CR>==
+-- nnoremap <A-k> :m .-2<CR>==
+-- inoremap <A-j> <Esc>:m .+1<CR>==gi
+-- inoremap <A-k> <Esc>:m .-2<CR>==gi
+-- vnoremap <A-j> :m '>+1<CR>gv=gv
+-- vnoremap <A-k> :m '<-2<CR>gv=gv
